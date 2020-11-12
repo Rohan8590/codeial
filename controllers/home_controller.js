3699b349071cb07e77f1de7112f7311d2e2ea1be
Rohan Aggarwal <rohanaggarwal8590@gmail.com>
2020-11-12 23:12:57 +0530

    added post form and post and author display and can only post if signed in

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,21 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    return res.render('home',{
-        title: "home"
+    //console.log(req.cookies);
+    //req.cookie('user_id',25);
+    // Post.find({}, function(err,posts){
+    //     return res.render('home',{
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+    
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
 }
+
---
+++ routes/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+//to check if user is signed in, only then he can send the post content to /create
+const passport = require('passport');
+
+const postsController = require('../controllers/posts_controller');
+
+//checkAuthentication is a function we created in passport-local-startegy in config
+router.post('/create', passport.checkAuthentication, postsController.create);
+
+module.exports = router;
