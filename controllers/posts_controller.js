fa731faa130cfad2550494a01a0b5a49a50a365d
Rohan Aggarwal <rohanaggarwal8590@gmail.com>
2020-11-19 16:15:55 +0530

    added ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,15 +3,31 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
    try{
-    await Post.create({
+    let post = await Post.create({
         content: req.body.content,
         user: req.user._id
     });
+
+    //determine if request is AJAX reqest. A type of AJAX request is xml http request
+    if(req.xhr){
+        // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
+        post = await post.populate('user', 'name').execPopulate();
+
+        return res.status(200).json({
+            data: {
+                post: post
+            },
+            message: "Post created!"
+        });
+    } 
+
     req.flash('success', 'Post created successfully');
     return res.redirect('back');
    }
    catch(err){
     req.flash('error', err);
+    // added this to view the error on console as well
+    console.log(err);
     return res.redirect('back');
    }
     
@@ -23,7 +39,18 @@ module.exports.destroy = async function(req,res){
         //id means conerting the object id(_id) to string automatically
         if(post.user == req.user.id){
             post.remove();
+
             await Comment.deleteMany({post: req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted successfully"
+                })
+            }
+
             req.flash('success', 'Post and it\'s comments deleted');
             return res.redirect('back');
         }
